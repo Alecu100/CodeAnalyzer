960b866ae5e7b12965be3d149c2bd9570d9abead
DESKTOP-8C812IN\alecs <alecu89@yahoo.com>
2017-11-11 22:36:31 +0200

    forgot to add some files

---
+++ CodeEvaluator.Evaluation/Members/EvaluatedPropertyObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using CodeEvaluator.Evaluation.Common;
+
+namespace CodeEvaluator.Evaluation.Members
+{
+    public class EvaluatedPropertyObject : EvaluatedObject
+    {
+        public EvaluatedPropertyObject(
+            EvaluatedTypeInfo referenceType,
+            EvaluatedObject evaluatedObject,
+            EvaluatedProperty targetProperty,
+            CodeEvaluatorExecutionStack evaluatorExecutionStack)
+        {
+            var evaluatedObjectReference = new EvaluatedObjectDirectReference();
+            evaluatedObjectReference.AssignEvaluatedObject(evaluatedObject);
+
+            _fields.Add(evaluatedObjectReference);
+            CodeEvaluatorExecutionStack = evaluatorExecutionStack;
+            TargetProperty = targetProperty;
+            TypeInfo = referenceType;
+        }
+
+        public EvaluatedProperty TargetProperty { get; }
+
+        public EvaluatedObject TargetObject => _fields.Count > 0 ? _fields[0].EvaluatedObjects[0] : null;
+
+        public override IReadOnlyList<EvaluatedObjectReference> Fields => _fields;
+
+        public override EvaluatedTypeInfo TypeInfo { get; }
+
+        public override List<EvaluatedObjectHistory> History => _fields[0].EvaluatedObjects[0].History;
+
+        public CodeEvaluatorExecutionStack CodeEvaluatorExecutionStack { get; }
+    }
+}
---
+++ CodeEvaluator.Evaluation/Members/EvaluatedPropertyObjectReference.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using CodeEvaluator.Evaluation.Extensions;
+using CodeEvaluator.Evaluation.Interfaces;
+using StructureMap;
+
+namespace CodeEvaluator.Evaluation.Members
+{
+    public class EvaluatedPropertyObjectReference : EvaluatedObjectReference
+    {
+        public override IReadOnlyList<EvaluatedObject> EvaluatedObjects
+        {
+            get
+            {
+                var evaluatedObjects = new List<EvaluatedObject>();
+
+                foreach (var evaluatedPropertyObject in EvaluatedPropertyObjects)
+                {
+                    var objects = CallGetValue(evaluatedPropertyObject);
+
+                    if (objects.IsNotNull())
+                        evaluatedObjects.AddRange(objects.EvaluatedObjects);
+                }
+
+                return evaluatedObjects;
+            }
+        }
+
+        public List<EvaluatedPropertyObject> EvaluatedPropertyObjects { get; } = new List<EvaluatedPropertyObject>();
+
+        private EvaluatedObjectReference CallGetValue(EvaluatedPropertyObject evaluatedPropertyObject)
+        {
+            var methodInvocationResolver = ObjectFactory.GetInstance<IMethodInvocationResolver>();
+            var syntaxNodeEvaluatorFactory = ObjectFactory.GetInstance<ISyntaxNodeEvaluatorFactory>();
+
+            var methodInvocationResolverResult = methodInvocationResolver.ResolveMethodInvocation(
+                new List<EvaluatedMethodBase> {evaluatedPropertyObject.TargetProperty.PropertyGetAccessor},
+                evaluatedPropertyObject.TargetObject, evaluatedPropertyObject.TypeInfo,
+                new List<EvaluatedObjectReference>(), new Dictionary<string, EvaluatedObjectReference>());
+
+            if (methodInvocationResolverResult.CanInvokeMethod)
+            {
+                var previousPassedArguments = new Dictionary<int, EvaluatedObjectReference>();
+
+                var passedMethodParameters = evaluatedPropertyObject.CodeEvaluatorExecutionStack.CurrentExecutionFrame
+                    .PassedMethodParameters;
+
+                foreach (var evaluatedObjectReference in passedMethodParameters)
+                    previousPassedArguments[evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+
+                passedMethodParameters.Clear();
+
+                foreach (var evaluatedObjectReference in methodInvocationResolverResult.ResolvedPassedParameters)
+                    evaluatedPropertyObject.CodeEvaluatorExecutionStack.CurrentExecutionFrame.PassedMethodParameters[
+                        evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+
+                var methodEvaluator =
+                    syntaxNodeEvaluatorFactory.GetSyntaxNodeEvaluator(
+                        methodInvocationResolverResult.ResolvedMethod.Declaration,
+                        EEvaluatorActions.None);
+
+                if (methodEvaluator != null)
+                    methodEvaluator.EvaluateSyntaxNode(
+                        methodInvocationResolverResult.ResolvedMethod.Declaration,
+                        evaluatedPropertyObject.CodeEvaluatorExecutionStack);
+
+                passedMethodParameters.Clear();
+
+                foreach (var evaluatedObjectReference in previousPassedArguments)
+                    passedMethodParameters[evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+
+                return evaluatedPropertyObject.CodeEvaluatorExecutionStack.CurrentExecutionFrame.MemberAccessReference;
+            }
+
+            return null;
+        }
+
+        public override void AssignEvaluatedObject(EvaluatedObject evaluatedObject)
+        {
+            foreach (var evaluatedPropertyObject in EvaluatedPropertyObjects)
+                CallSetValue(evaluatedPropertyObject, evaluatedObject);
+        }
+
+        private void CallSetValue(EvaluatedPropertyObject evaluatedPropertyObject, EvaluatedObject evaluatedObject)
+        {
+            var methodInvocationResolver = ObjectFactory.GetInstance<IMethodInvocationResolver>();
+            var syntaxNodeEvaluatorFactory = ObjectFactory.GetInstance<ISyntaxNodeEvaluatorFactory>();
+
+            var methodInvocationResolverResult = methodInvocationResolver.ResolveMethodInvocation(
+                new List<EvaluatedMethodBase> {evaluatedPropertyObject.TargetProperty.PropertySetAccessor},
+                evaluatedPropertyObject.TargetObject, evaluatedPropertyObject.TypeInfo,
+                new List<EvaluatedObjectReference> {new EvaluatedObjectDirectReference(evaluatedObject)},
+                new Dictionary<string, EvaluatedObjectReference>());
+
+            if (methodInvocationResolverResult.CanInvokeMethod)
+            {
+                var previousPassedArguments = new Dictionary<int, EvaluatedObjectReference>();
+
+                var passedMethodParameters = evaluatedPropertyObject.CodeEvaluatorExecutionStack.CurrentExecutionFrame
+                    .PassedMethodParameters;
+
+                foreach (var evaluatedObjectReference in passedMethodParameters)
+                    previousPassedArguments[evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+
+                passedMethodParameters.Clear();
+
+                foreach (var evaluatedObjectReference in methodInvocationResolverResult.ResolvedPassedParameters)
+                    evaluatedPropertyObject.CodeEvaluatorExecutionStack.CurrentExecutionFrame.PassedMethodParameters[
+                        evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+
+                var methodEvaluator =
+                    syntaxNodeEvaluatorFactory.GetSyntaxNodeEvaluator(
+                        methodInvocationResolverResult.ResolvedMethod.Declaration,
+                        EEvaluatorActions.None);
+
+                if (methodEvaluator != null)
+                    methodEvaluator.EvaluateSyntaxNode(
+                        methodInvocationResolverResult.ResolvedMethod.Declaration,
+                        evaluatedPropertyObject.CodeEvaluatorExecutionStack);
+
+                passedMethodParameters.Clear();
+
+                foreach (var evaluatedObjectReference in previousPassedArguments)
+                    passedMethodParameters[evaluatedObjectReference.Key] = evaluatedObjectReference.Value;
+            }
+        }
+
+        public override void AssignEvaluatedObject(EvaluatedObjectReference evaluatedObjectReference)
+        {
+            foreach (var evaluatedObject in evaluatedObjectReference.EvaluatedObjects)
+                AssignEvaluatedObject(evaluatedObject);
+        }
+
+        public override void AssignEvaluatedObjects(IEnumerable<EvaluatedObject> evaluatedObjects)
+        {
+            foreach (var evaluatedObject in evaluatedObjects)
+                AssignEvaluatedObject(evaluatedObject);
+        }
+    }
+}
